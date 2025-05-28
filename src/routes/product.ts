0a9122d6781c9df115dd4e80ccdca3a60691bc1e
htoomyatnyinyi <htoomyatnyinyi@gmail.com>
2025-05-28 19:36:37 +0700

    product crud api

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -9,8 +9,8 @@ import { authenticated, permission } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
-router.post("/new-product", authenticated, createProduct);
 router.get("/", getProducts);
+router.post("/", authenticated, createProduct);
 router.get("/:id", authenticated, permission(["USER"]), getProductById);
 router.delete("/:id", authenticated, permission(["USER"]), deleteProduct);
 
