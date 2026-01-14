ec82c4a4174a6b88b88071aa09fb4f9ea697755c
Htoo Myat Nyi Nyi <htoomyatnyinyi@fedora.local>
2026-01-14 14:58:44 +0700

    .env secrep remove

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -15,14 +15,9 @@ const EMAIL_PASS: string = process.env.EMAIL_PASS || "ggix chag wcui jgxp";
 const FRONTEND_URL: string =
   process.env.FRONTEND_URL || "http://localhost:5173";
 
-const STRIPE_SECRET_KEY: string =
-  process.env.STRIPE_SECRET_KEY ||
-  "sk_test_51SpOXN5a8dgiY5gLDYwZFj3185FaE7gW878nuB89oghjWKLgpRlFcETOZaAwsbi4tHKBvLXvFFZFwww4NpkBmjvd00mIIEyDDU";
-const STRIPE_WEBHOOK_SECRET: string =
-  process.env.STRIPE_WEBHOOK_SECRET || "whsec_1234567890abcdef1234567890abcdef";
-const STRIPE_PUBLISHABLE_KEY: string =
-  process.env.STRIPE_PUBLISHABLE_KEY ||
-  "pk_test_51SSorfHbhN0Z6COOSbTbqgRAM1kXRQqzte0sxKw0NIKsUELB4YMTiyyXS4KOdoDC288MwYwghM2Pj5Ro61U04WCU00IFeNFn3a";
+const STRIPE_SECRET_KEY: string = process.env.STRIPE_SECRET_KEY || "";
+const STRIPE_WEBHOOK_SECRET: string = process.env.STRIPE_WEBHOOK_SECRET || "";
+const STRIPE_PUBLISHABLE_KEY: string = process.env.STRIPE_PUBLISHABLE_KEY || "";
 
 export {
   PORT,
